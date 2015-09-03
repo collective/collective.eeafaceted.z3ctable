f41453d8441d319b4ac9f6173c486b1f4a8ccd79
Gauthier Bastien <g.bastien@imio.be>
2015-09-03 14:02:30 +0200

    Added test for table.render_row to show that a column may now define a CSS for the <tr>

--- src/collective/eeafaceted/z3ctable/tests/test_table.py
+++ src/collective/eeafaceted/z3ctable/tests/test_table.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import lxml.html
+from collective.eeafaceted.z3ctable.columns import BaseColumn
 from collective.eeafaceted.z3ctable.testing import IntegrationTestCase
 from eea.facetednavigation.interfaces import ICriteria
 from plone.batching import Batch
@@ -39,3 +40,21 @@ class TestTable(IntegrationTestCase):
         # the brain is actually displayed in the table
         brain = brains[0]
         self.assertEquals(rendered_table.find('tbody').find('tr').find('td').text_content(), brain.Title)
+
+    def test_Table_CSS_on_tr_from_cell(self, ):
+        """table.renderRow was overrided to take into account 'tr' CSS classes defined on a column."""
+        table = self.faceted_z3ctable_view
+        column = BaseColumn(self.portal, self.portal.REQUEST, table)
+        column.attrName = 'Title'
+        table.nameColumn(column, 'Title')
+        # build a Batch
+        brains = self.portal.portal_catalog(portal_type='Folder')
+        batch = Batch(brains, size=5)
+        # adapt css defined for column to change <tr> applied CSS
+        column.getCSSClasses = lambda x: {'tr': 'special_tr_class'};
+        # ok, now make table.setUpColumns take our configured column
+        self.portal.REQUEST.set('column', column)
+        table.setUpColumns = lambda *x: [__import__('zope').component.hooks.getSite().REQUEST.get('column'), ]
+        rendered_table = lxml.html.fromstring(table.render_table(batch))
+        # the class is applied to the <tr>, in addition to the 'odd' class
+        self.assertEquals(rendered_table.find('tbody').find('tr').attrib['class'], 'odd special_tr_class')
