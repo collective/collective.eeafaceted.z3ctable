2ab6cac02b34b4e3baec2dbeed97a58643d211ff
Steffen Lindner <mail@steffen-lindner.de>
2023-10-24 13:25:37 +0200

    Works with eea.facetednavigation v16 and newer

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         'Plone',
         'collective.excelexport',
-        'eea.facetednavigation<16.0',
+        'eea.facetednavigation>=16.0',
         'imio.helpers',
         'plone.api>=1.3.0',
         'plone.formwidget.namedfile>=2.0.2',
